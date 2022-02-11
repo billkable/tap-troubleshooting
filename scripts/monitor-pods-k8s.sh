94594eed1a52d92fd25f726b620f78ded97d0151
Bill Kable <bkable@gmail.com>
2022-02-10 21:35:44 -0600

    add helper scripts

--- scripts/monitor-pods-k8s.sh
+++ scripts/monitor-pods-k8s.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 watch kubectl get pods
--- scripts/monitor-workload-resources.sh
+++ scripts/monitor-workload-resources.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 watch kubectl get workload,gitrepository,pipelinerun,images.kpack,podintent,app,services.serving
